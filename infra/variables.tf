0c23f38d8cc5ea13d9ce9837a52ca6eea10100de
Antonio <guarniericomputador@gmail.com>
2023-06-26 23:13:04 -0300

    DEV-1 ALTERAÇÃO DO CODIGO ANTIGO PARA O NOVO

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,13 +1,7 @@
-
-variable "resource_group_name" {
-  #default = "myTFResourceGroup"
 variable "nome_resource_group" {
 
 }
 
-#variable "regiao_azure" {
-#
-#}
 variable "nome_service_plan" {
 
 }
